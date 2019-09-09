103bcae780014d27392f4916f93cd38ad0de12c2
Itamar Tati <itamar.tati1994@gmail.com>
2019-09-09 14:36:25 +0100

    Updates

---
+++ precache-manifest.d6ad5f6ad88b9fd9c7723c6b84a6886a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69c346928603ce530e4a6065d85ca83",
+    "url": "/david-portfolio/index.html"
+  },
+  {
+    "revision": "3b590ab26d9cacb46e41",
+    "url": "/david-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a7bbf09717f17f563078",
+    "url": "/david-portfolio/static/js/2.969fbc36.chunk.js"
+  },
+  {
+    "revision": "3b590ab26d9cacb46e41",
+    "url": "/david-portfolio/static/js/main.e9475f04.chunk.js"
+  },
+  {
+    "revision": "0105b847933998095193",
+    "url": "/david-portfolio/static/js/runtime~main.687911bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/david-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
